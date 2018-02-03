50dab229303b7b8fc99a1956eadb14a0c4888945
Nadira Fathima <nadirafathima1@gmail.com>
2018-02-03 18:01:58 -0500

    Search bar input event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const SearchBar = () =>{
-    return <input />; // import react for jsx as jsx is converted into React.createElement
-};
+//{Component} similar to const Component = React.Component
 
-export default SearchBar;
+// const SearchBar = () =>{
+//     return <input />; // import react for jsx as jsx is converted into React.createElement
+// };
+
+export default class SearchBar extends Component{
+    render(){  //every class based react component must have a defined render method
+        return <input onChange={this.onInputChange} />;
+    }
+
+    //event handler
+    onInputChange(eventObject){ //eventObject describes the context and a lot of information about the event that occured.
+        console.log(eventObject.target.value)
+    }
+}
